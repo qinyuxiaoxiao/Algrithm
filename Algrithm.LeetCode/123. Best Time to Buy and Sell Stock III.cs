ab11dc07085031e04f99100a7b539747f06ddd71
qinyuxiaoxiao <qinyuxiaoxiao@hotmail.com>
2016-11-25 22:39:25 +0800

    Finshed 122. Best time to buy and sell stock III.

--- Algrithm.LeetCode/123. Best Time to Buy and Sell Stock III.cs
+++ Algrithm.LeetCode/123. Best Time to Buy and Sell Stock III.cs
@@ -14,40 +14,39 @@ namespace Algrithm.LeetCode
         You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
     */
     /// <summary>
-    /// 关键：只需累加增长的部分
+    /// 关键：理解题意，第二次买入再卖出总是建立在第一次买入再卖出之后，顺着该思路来设计。
+    /// 参考答案的方法很巧妙，理解起来比较困难。最好画一个真实的价格走势图，按照其算法算出每一步的变量值，从而理解其算法逻辑
+    /// 
+    /// 把两次买入再卖出的先后操作细拆分为4个顺序发生的操作，用4个变量记录每步操作后的收益：
+    /// 
+    ///     profit1Buy      第1次买入(没有卖出)时的收益
+    ///     profit1Sell     第1次卖出时的收益
+    ///     profit2Buy      第2次买入(没有卖出)时的收益
+    ///     profit2Sell     第2次卖出时的收益
+    ///     
+    /// 注意：计算的顺是自后向前计算，即按照profit2Sell， profit2Buy， profit1Sell， profit1Buy的顺序来计算
+    ///     
     /// </summary>
     class BestTimeToBuyAndSellStockIII
     {
         private static int GetMaximumProfit(int[] prices)
         {
             if (prices == null || prices.Length <= 1) return 0;
-
-            int maxProfit1 = 0;
-            int maxProfit2 = 0;
-            int profit = 0;
-            for(int i = 1; i < prices.Length; i++)
+            int profit1Buy = int.MinValue;
+            int profit1Sell = 0;
+            int profit2Buy = int.MinValue;
+            int profit2Sell = 0;
+            for (int i = 0; i < prices.Length; i++)
             {
-                if (prices[i] > prices[i] - 1)
-                {
-                    profit += prices[i] - prices[i - 1];
-                }
-                else
-                {
-                    if (profit > maxProfit1)
-                    {
-                        maxProfit1 = profit;
-                        maxProfit2 = Math.Max(maxProfit1, maxProfit2);
-                    }
-                    else if (profit > maxProfit2)
-                    {
-                        maxProfit2 = profit;
-                    }
-
-                    profit = 0;
-                }
+                // 注意：计算的顺是自后向前计算，即按照profit2Sell， profit2Buy， profit1Sell， profit1Buy的顺序来计算
+                profit2Sell = Math.Max(profit2Sell, profit2Buy + prices[i]);
+                profit2Buy = Math.Max(profit2Buy, profit1Sell - prices[i]);
+                profit1Sell = Math.Max(profit1Sell, profit1Buy + prices[i]);
+                profit1Buy = Math.Max(profit1Buy, -prices[i]);
             }
 
-            return maxProfit1 + maxProfit2;
+            // 取最大值是因为可能操作1次取得最大收益
+            return Math.Max(profit2Sell, profit1Sell);
         }
 
         static void Main(string[] args)
